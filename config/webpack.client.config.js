72c89124094cdd4c4ab87c2d5c4eaf44cea1c07a
fhac-koh <umwdream2017@gmail.com>
2022-01-12 13:58:20 +0900

    initialize commit

---
+++ config/webpack.client.config.js
@@ -0,0 +1,36 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const path = require("path");
+
+const htmlWebPackPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html'
+});
+
+const dotenv = new Dotenv()
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader'
+        }
+      },
+    ]
+  },
+  plugins: [
+      dotenv,
+      htmlWebPackPlugin
+    ]
+};
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Hello } from "./Hello";
+
+export const App: React.FC = () => {
+    return (
+        <div>
+            <Hello name={"test"} />
+        </div>
+    );
+};
